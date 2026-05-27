a6d297faf5448774ea744c89ea15a93c13859d39
Akash Gurav <140896056+Gurav54@users.noreply.github.com>
2026-05-28 00:14:33 +0530

    For loop (1), BREAK | CONTINUE | ELSE  + problem solving
    Practiced Python for loops with break, continue, and else.
    Solved beginner loop exercises using user input.
    Printed numbers, reverse counting, multiplication table, and sum of natural numbers.
    Focused on understanding loop flow and iteration basics.

--- ForLoopChapter8.py
+++ ForLoopChapter8.py
@@ -20,12 +20,68 @@ for i in range (input ,(input*10)+1 ,input):
 """
 
 # For loop on single string 
+"""
 a="Apple"
 for i in a:
     print (i)
+"""
 
 # For loop , getting String form user and print it 
+"""
 name=input("Enter your Name")
 
 for i in range(len(name)):
     print(f"{i} : {name[i]}")
+ """
+
+#### For loop , BREAK | CONTINUE | ELSE #######
+
+# For loop break example 
+for i in range (1,11,1):
+    if i == 5:
+        break
+    print(i)
+
+for i in range(1,11,1):
+    if i ==6:
+        continue
+    print(i)
+
+# ELSE in For loop
+#The else block runs only if the loop finishes entirely without hitting a break.
+
+for i in range(1,21,1):
+    if i == 69:
+        break
+    print(i)
+else:
+    print("FOR LOOPS didnt Hit break statement")
+
+#Task 1 : Print Hello world nth time 
+inputFromUser=int(input("Enter how many time you want to print Hello world:-"))
+for i in range(1,inputFromUser+1,1):
+    print(f"{i} Hello World")
+
+# Task 2 : print natural numbers based on N th value 
+givenNumber=int(input ("Enter your number :-"))
+for i in range(1,givenNumber+1):
+    print(i)
+
+# Task 3 : print the numbers in reverse order , get input from user 
+number=int (input("Enter your Number for reverse printing :-"))
+for i in range(number , 0,-1):
+    print(i)
+
+#Task 4 : print the 5th table , output should be 5x1=5 , 5x2=10 ....
+
+number=int(input("Enter your Number for table print :-"))
+for i in range(1,11,1):
+    print(f"{number} x {i}={number*i}")
+    #print(number*i)
+
+#Task 5 : print the addition of natural numbers
+number=int(input("Enter your number for addition :-"))
+sum=0
+for i in range(1,number+1):
+    sum=i+number
+print(sum)
